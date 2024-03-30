481c278981e48bf7ba8b0f98d555466a2888b4b8
user <user@WIN-AJ1NUT8T8HT>
2024-03-30 14:33:16 +0300

    Добавьте файлы проекта.

---
+++ SiteGenerator.Domain/Entities/Website.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SiteGenerator.Domain.Entities
+{
+    public class Website
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] 
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("age")]
+        public string Age { get; set; }
+    }
+}
---
+++ SiteGenerator/Controllers/SitesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using SiteGenerator.Domain.Entities;
+using SiteGenerator.Infrastructure.Data;
+
+namespace SiteGenerator.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SitesController(MongoDbService mongoDbService) : ControllerBase
+    {
+        private readonly IMongoCollection<Website>? _websites = mongoDbService.Database?.GetCollection<Website>("websites");
+
+        [HttpGet]
+        public async Task<IEnumerable<Website>> Get()
+        {
+            return await _websites.Find(FilterDefinition<Website>.Empty).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Website?>> GetById(string id)
+        {
+            var filter = Builders<Website>.Filter.Eq(x => x.Id, id);
+            var test = await _websites.FindAsync(filter);
+            var item = test.FirstOrDefault();
+            return test is not null ? Ok(test) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(string name)
+        {
+            var test = new Website() { Name = name };
+            await _websites.InsertOneAsync(test);
+            return CreatedAtAction(nameof(GetById), new { id = test.Id }, test);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Website test)
+        {
+            var filter = Builders<Website>.Filter.Eq(x => x.Id, test.Id);
+            //var update = Builders<Test>.Update
+            //    .Set(x => x.Name, test.Name);
+            //await _test.UpdateOneAsync(filter, update);
+            await _websites.ReplaceOneAsync(filter, test);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var filter = Builders<Website>.Filter.Eq(x => x.Id, id);
+            await _websites.DeleteOneAsync(filter);
+            return Ok();
+        }
+    }
+}
