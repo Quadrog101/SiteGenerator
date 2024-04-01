b76527cc640d1ad5ce7a0a8feb6bc4e71ec9f9c3
DimDey <dimdey.deyna@gmail.com>
2024-04-01 21:17:02 +0300

    News service orderby, refactor code

--- src/SiteGenerator.Domain/Services/NewsService.cs
+++ src/SiteGenerator.Domain/Services/NewsService.cs
@@ -3,10 +3,8 @@ using MongoDB.Driver;
 using SiteGenerator.Domain.Abstractions;
 using SiteGenerator.Domain.Models;
 using MongoDB.Driver.Linq;
-using MongoDB.Bson;
 using SiteGenerator.Domain.Entities;
 using SiteGenerator.Domain.Exceptions;
-using System.Text.Json;
 
 namespace SiteGenerator.Domain.Services
 {
@@ -14,13 +12,11 @@ namespace SiteGenerator.Domain.Services
     {
         private readonly IApplicationContext _context;
         private readonly IMapper _mapper;
-        // private readonly IHttpContextAccessor _httpContextAccessor;
 
         public NewsService(IApplicationContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
-            // _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<IEnumerable<NewsModel>> GetList(string alias, int pageNumber = 0,
@@ -31,6 +27,7 @@ namespace SiteGenerator.Domain.Services
                 .Where(x => x.Alias == alias)
                 .Skip(pageSize * pageNumber)
                 .Take(pageSize)
+                .OrderByDescending(site => site.Created)
                 .ToListAsync(cancellationToken);
 
             return _mapper.Map<IEnumerable<NewsModel>>(news);
@@ -38,29 +35,22 @@ namespace SiteGenerator.Domain.Services
 
         public async Task<NewsModel> CreateNews(CreateNewsModel model, CancellationToken cancellationToken)
         {
-            //if (await _context.News.AsQueryable().AnyAsync(site => site.OwnerId == 1, cancellationToken: cancellationToken))
-            //{
-            //    throw new BusinessException("Пользователь уже является владельцем сайта");
-            //}
-
-            if (await _context.Websites.AsQueryable().AnyAsync(site => site.Alias == model.Alias, cancellationToken: cancellationToken))
-            {
-                var news = new News
-                {
-                    Alias = model.Alias,
-                    Title = model.Title,
-                    Image = model.Image,
-                    Description = model.Description,
-                    Created = DateTime.Now,
-                    // OwnerId = _httpContextAccessor.HttpContext.User
-                };
-
-                await _context.News.InsertOneAsync(news, cancellationToken: cancellationToken);
-
-                return _mapper.Map<NewsModel>(news);
-            }
-            else
+            if (!await _context.Websites.AsQueryable().AnyAsync(site => site.Alias == model.Alias, cancellationToken: cancellationToken))
                 throw new BusinessException("Нет такого новостного сайта");
+            
+            var news = new News
+            {
+                Alias = model.Alias,
+                Title = model.Title,
+                Image = model.Image,
+                Description = model.Description,
+                Created = DateTime.Now,
+                // OwnerId = _httpContextAccessor.HttpContext.User
+            };
+
+            await _context.News.InsertOneAsync(news, cancellationToken: cancellationToken);
+
+            return _mapper.Map<NewsModel>(news);
         }
     }
 }
